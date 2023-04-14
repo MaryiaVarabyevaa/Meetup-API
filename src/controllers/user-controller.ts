e83aa9eb1deffc74aa9a44b6329e075b3e7f8f81
Varabyova Maryia <varabyovamaryia@Varabyovas-MacBook-Air.local>
2023-04-14 20:30:55 +0300

    fix: improved type for UserController and UserService

--- src/controllers/user-controller.ts
+++ src/controllers/user-controller.ts
@@ -1,9 +1,13 @@
+import { Request, Response, NextFunction } from 'express';
 import userService from "../services/user-service";
+import {NextFunction} from "express";
+import {AuthenticatedRequest} from "../types/AuthenticatedRequest";
+import {CreateUser} from "../types/CreateUser";
 
 class UserController {
-    async registration(req, res, next) {
+    async registration(req: AuthenticatedRequest, res: Response, next: NextFunction): Promise<Response> {
         try {
-            const userData = await userService.registration(req.validatedData);
+            const userData = await userService.registration(req.userValidatedData as CreateUser);
             // срок хранения куки 30 дней
             res.cookie('refreshToken', userData.refreshToken, {maxAge: 30 * 24 * 60 * 60 * 1000, httpOnly: true});
             return res.status(201).json(userData);
@@ -12,7 +16,7 @@ class UserController {
         }
     }
 
-    async login(req, res, next) {
+    async login(req: Request, res: Response, next: NextFunction): Promise<Response> {
         try {
             const {email, password} = req.body;
             const userData = await userService.login(email, password);
@@ -23,9 +27,9 @@ class UserController {
         }
     }
 
-    async refresh(req, res, next) {
+    async refresh(req: Request, res: Response, next: NextFunction): Promise<Response>  {
         try {
-            const { refreshToken } = req.cookie;
+            const { refreshToken } = req.cookies;
             const userData = await userService.refresh(refreshToken);
             res.cookie('refreshToken', userData.refreshToken, {maxAge: 30 * 24 * 60 * 60 * 1000, httpOnly: true});
             return res.status(200).json(userData);
@@ -34,10 +38,10 @@ class UserController {
         }
     }
 
-    async changeUserRole(req, res, next) {
+    async changeUserRole(req: Request, res: Response, next: NextFunction): Promise<Response>  {
         try {
             const { id } = req.params;
-            const organizer = await userService.changeUserRole(id);
+            const organizer = await userService.changeUserRole(+id);
             return res.status(200).json(organizer);
         } catch (err) {
             next(err);
--- src/services/user-service.ts
+++ src/services/user-service.ts
@@ -4,11 +4,18 @@ import tokenService from "../services/token-service";
 import ApiError from "../exceptions/api-error";
 import TokenPayload from "../utils/jwtUtils";
 import {UserRole} from "../constants/userRoles";
+import {TokenPair} from "../types/TokenPair";
 
 const { User } = require('../models/user-model');
 
 class UserService {
-    async registration(userDto: CreateUser) {
+    private async generateTokens(user): Promise<TokenPair> {
+        const payload = new TokenPayload(user);
+        const tokens = tokenService.generateToken({...payload});
+        return { ...tokens };
+    }
+
+    async registration(userDto: CreateUser): Promise<TokenPair> {
         const { email, password, firstName, lastName } = userDto;
         const candidate = await User.findOne({where: {email}});
         if (candidate) {
@@ -16,36 +23,23 @@ class UserService {
         }
         const hashPassword = await bcrypt.hash(password, 3);
         const user = await User.create({email, password: hashPassword, first_name: firstName, last_name: lastName});
-        const payload = new TokenPayload(user);
-        const tokens = tokenService.generateToken({...payload});
-        await tokenService.saveToken(payload.id, tokens.refreshToken);
-
-        return {
-            ...tokens,
-            user: payload
-        }
+        return this.generateTokens(user);
     }
 
-    async login(email: string, password: string) {
+    async login(email: string, password: string): Promise<TokenPair> {
         const user = await User.findOne({where: {email}});
         if (!user) {
             throw ApiError.BadRequest('Invalid email or password. Please check your credentials and try again.');
         }
-        const isPasswordEquals = bcrypt.compare(password, user.password);
+        const {password: userPassword} = user;
+        const isPasswordEquals = await bcrypt.compare(password, userPassword as string);
         if (!isPasswordEquals) {
             throw ApiError.BadRequest('Invalid email or password. Please check your credentials and try again.')
         }
-        const payload = new TokenPayload(user);
-        const tokens = tokenService.generateToken({...payload});
-        await tokenService.saveToken(payload.id, tokens.refreshToken);
-
-        return {
-            ...tokens,
-            user: payload
-        }
+        return this.generateTokens(user);
     }
 
-    async refresh(refreshToken: string) {
+    async refresh(refreshToken: string): Promise<TokenPair> {
         if (!refreshToken) {
             throw ApiError.UnauthorizedError();
         }
@@ -55,25 +49,20 @@ class UserService {
             throw ApiError.UnauthorizedError();
         }
 
-        const user = await User.findOne({where: {id: userData.id}})
-        const payload = new TokenPayload(user);
-        const tokens = tokenService.generateToken({...payload});
-        await tokenService.saveToken(payload.id, tokens.refreshToken);
-
-        return {
-            ...tokens,
-            user: payload
-        }
+        const user = await User.findOne({where: {id: userData.id}});
+        return this.generateTokens(user);
     }
 
-    async changeUserRole(id: number) {
+    async changeUserRole(id: number): Promise<void> {
         const user = await User.findOne({where: {id}});
         if (!user) {
             throw ApiError.NotFound();
         }
-        const role = UserRole.USER === user.role? UserRole.ORGANIZER : UserRole.USER;
-        const organizer = User.update({role}, {where: {id}});
-        return organizer;
+        const { role} = user;
+        const newRole = UserRole.USER === role? UserRole.ORGANIZER : UserRole.USER;
+        await user.update( { role: newRole });
+        await user.save();
+        return;
     }
 }
 
