a39646bd9ecfd9fb33cf60f1e0ac88e5bff1d963
Varabyova Maryia <varabyovamaryia@Varabyovas-MacBook-Air.local>
2023-04-14 10:36:40 +0300

    fix: replace console.log with error middleware

--- src/controllers/meetup-controller.ts
+++ src/controllers/meetup-controller.ts
@@ -3,26 +3,26 @@ import ApiError from "../exceptions/api-error";
 import {UserRole} from "../constants/userRoles";
 
 class MeetupController {
-    async findAllMeetups(req, res) {
+    async findAllMeetups(req, res, next) {
         try {
             const meetups = await meetupService.findAllMeetups(req.query);
             return res.status(200).json(meetups);
         } catch (err) {
-            console.log(err);
+            next(err);
         }
     }
 
-    async findMeetupById(req, res) {
+    async findMeetupById(req, res, next) {
         try {
            const { id } = req.params;
            const meetup = await meetupService.findMeetupById(id);
            res.status(200).json(meetup)
         } catch (err) {
-            console.log(err);
+            next(err);
         }
     }
 
-    async addMeetup(req, res) {
+    async addMeetup(req, res, next) {
         try {
             const { role } = req.validatedData;
             if (role === UserRole.USER) {
@@ -31,11 +31,11 @@ class MeetupController {
             const meetup = await meetupService.addMeetup(req.validatedData);
             res.status(201).json(meetup);
         } catch (err) {
-            console.log(err);
+            next(err);
         }
     }
 
-    async updateMeetup(req, res) {
+    async updateMeetup(req, res, next) {
         try {
             const { role, accessingUserId, userId } = req.validatedData;
             if (role === UserRole.USER && accessingUserId !== userId) {
@@ -45,10 +45,10 @@ class MeetupController {
             res.status(200).json(updatedMeetup);
 
         } catch (err) {
-            console.log(err);
+            next(err);
         }
     }
-    async deleteMeetup(req, res) {
+    async deleteMeetup(req, res, next) {
         try {
             const { role, id } = req.user;
             if (role === UserRole.USER) {
@@ -57,7 +57,7 @@ class MeetupController {
             const deletedMeetup = await meetupService.deleteMeetup(req.body.id, id);
             res.status(204).json(deletedMeetup);
         } catch (err) {
-            console.log(err);
+            next(err);
         }
     }
 }
