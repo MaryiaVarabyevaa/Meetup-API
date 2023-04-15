ef7aaf91c4a093a061d35143c1bae3222b54f6a1
Varabyova Maryia <varabyovamaryia@Varabyovas-MacBook-Air.local>
2023-04-15 11:53:36 +0300

    fix: update user service

--- src/services/user-service.ts
+++ src/services/user-service.ts
@@ -10,59 +10,80 @@ const { User } = require('../models/user-model');
 
 class UserService {
     private async generateTokens(user): Promise<TokenPair> {
-        const payload = new TokenPayload(user);
-        const tokens = tokenService.generateToken({...payload});
-        return { ...tokens };
+        try {
+            const payload = new TokenPayload(user);
+            const tokens = tokenService.generateToken({...payload});
+            await tokenService.saveToken(user.id, tokens.refreshToken);
+            return { ...tokens };
+        } catch (err) {
+            throw err;
+        }
     }
 
     async registration(userDto: CreateUser): Promise<TokenPair> {
-        const { email, password, firstName, lastName } = userDto;
-        const candidate = await User.findOne({where: {email}});
-        if (candidate) {
-            throw ApiError.BadRequest(`A user with ${email} email already exists`);
+        try {
+            const { email, password, firstName, lastName } = userDto;
+            const candidate = await User.findOne({where: {email}});
+            if (candidate) {
+                throw ApiError.BadRequest(`A user with ${email} email already exists`);
+            }
+            const hashPassword = await bcrypt.hash(password, 3);
+            const user = await User.create({email, password: hashPassword, first_name: firstName, last_name: lastName});
+            return this.generateTokens(user);
+        } catch (err) {
+            throw err;
         }
-        const hashPassword = await bcrypt.hash(password, 3);
-        const user = await User.create({email, password: hashPassword, first_name: firstName, last_name: lastName});
-        return this.generateTokens(user);
     }
 
     async login(email: string, password: string): Promise<TokenPair> {
-        const user = await User.findOne({where: {email}});
-        if (!user) {
-            throw ApiError.BadRequest('Invalid email or password. Please check your credentials and try again.');
+        try {
+            const user = await User.findOne({where: {email}});
+            if (!user) {
+                throw ApiError.BadRequest('Invalid email or password. Please check your credentials and try again.');
+            }
+            const {password: userPassword} = user;
+            const isPasswordEquals = await bcrypt.compare(password, userPassword as string);
+            if (!isPasswordEquals) {
+                throw ApiError.BadRequest('Invalid email or password. Please check your credentials and try again.')
+            }
+            return this.generateTokens(user);
+        } catch (err) {
+            throw err;
         }
-        const {password: userPassword} = user;
-        const isPasswordEquals = await bcrypt.compare(password, userPassword as string);
-        if (!isPasswordEquals) {
-            throw ApiError.BadRequest('Invalid email or password. Please check your credentials and try again.')
-        }
-        return this.generateTokens(user);
     }
 
     async refresh(refreshToken: string): Promise<TokenPair> {
-        if (!refreshToken) {
-            throw ApiError.UnauthorizedError();
-        }
-        const userData = tokenService.validateRefreshToken(refreshToken);
-        const tokenFromDb = await tokenService.findToken(refreshToken);
-        if (!userData || !tokenFromDb) {
-            throw ApiError.UnauthorizedError();
-        }
+        try {
+            if (!refreshToken) {
+                throw ApiError.UnauthorizedError();
+            }
+            const userData = tokenService.validateRefreshToken(refreshToken);
+            const tokenFromDb = await tokenService.findToken(refreshToken);
+            if (!userData || !tokenFromDb) {
+                throw ApiError.UnauthorizedError();
+            }
 
-        const user = await User.findOne({where: {id: userData.id}});
-        return this.generateTokens(user);
+            const user = await User.findOne({where: {id: userData.id}});
+            return this.generateTokens(user);
+        } catch (err) {
+            throw err;
+        }
     }
 
     async changeUserRole(id: number): Promise<void> {
-        const user = await User.findOne({where: {id}});
-        if (!user) {
-            throw ApiError.NotFound();
+        try {
+            const user = await User.findOne({where: {id}});
+            if (!user) {
+                throw ApiError.NotFound();
+            }
+            const { role} = user;
+            const newRole = UserRole.USER === role? UserRole.ORGANIZER : UserRole.USER;
+            await user.update( { role: newRole });
+            await user.save();
+            return;
+        } catch (err) {
+            throw err;
         }
-        const { role} = user;
-        const newRole = UserRole.USER === role? UserRole.ORGANIZER : UserRole.USER;
-        await user.update( { role: newRole });
-        await user.save();
-        return;
     }
 }
 
