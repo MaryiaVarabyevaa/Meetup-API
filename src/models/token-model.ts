2e85ca8e6a5e1ec20dbc790ac38fc26aad1425db
Varabyova Maryia <varabyovamaryia@Varabyovas-MacBook-Air.local>
2023-04-15 11:49:59 +0300

    fix: update Token model

--- src/models/token-model.ts
+++ src/models/token-model.ts
@@ -7,6 +7,19 @@ export const Token = sequelize.define('token', {
     refresh_token: {type: DataTypes.STRING, allowNull: false},
 })
 
+Token.addHook('afterCreate', (instance, options) => {
+    delete instance.dataValues.createdAt;
+    delete instance.dataValues.updatedAt;
+    return instance;
+});
+
+Token.addHook('afterFind', (result, options) => {
+    if (result) {
+        delete result.dataValues.createdAt;
+        delete result.dataValues.updatedAt;
+    }
+});
+
 module.exports = {
     Token
 }
