709653d3b29603f690a69fa5c2ab10839b3dd0c1
Varabyova Maryia <varabyovamaryia@Varabyovas-MacBook-Air.local>
2023-04-15 10:54:19 +0300

    fix: improved type for meetup controller

--- src/controllers/meetup-controller.ts
+++ src/controllers/meetup-controller.ts
@@ -1,61 +1,68 @@
+import { Request, Response, NextFunction } from 'express';
 import meetupService from "../services/meetup-service";
 import ApiError from "../exceptions/api-error";
 import {UserRole} from "../constants/userRoles";
+import {NextFunction} from "express";
+import {AuthenticatedRequest} from "../types/AuthenticatedRequest";
+import {CreateMeetup} from "../types/CreateMeetup";
+import {MeetUpSearchQuery} from "../types/MeetUpSearchQuery";
+import {UpdateMeetup} from "../types/UpdateMeetup";
 
 class MeetupController {
-    async findAllMeetups(req, res, next) {
+    async findAllMeetups(req: Request, res: Response, next: NextFunction): Promise<Response>   {
         try {
-            const meetups = await meetupService.findAllMeetups(req.query);
+            const meetups = await meetupService.findAllMeetups(req.query as MeetUpSearchQuery);
             return res.status(200).json(meetups);
         } catch (err) {
             next(err);
         }
     }
 
-    async findMeetupById(req, res, next) {
+    async findMeetupById(req: Request, res: Response, next: NextFunction): Promise<Response>   {
         try {
            const { id } = req.params;
-           const meetup = await meetupService.findMeetupById(id);
-           res.status(200).json(meetup)
+           const meetup = await meetupService.findMeetupById(+id);
+           return res.status(200).json(meetup)
         } catch (err) {
             next(err);
         }
     }
 
-    async addMeetup(req, res, next) {
+    async addMeetup(req: AuthenticatedRequest, res: Response, next: NextFunction): Promise<Response>   {
         try {
-            const { role } = req.validatedData;
+            const { role, id } = req.user;
             if (role === UserRole.USER) {
                 throw ApiError.Forbidden();
             }
-            const meetup = await meetupService.addMeetup(req.validatedData);
-            res.status(201).json(meetup);
+            const meetup = await meetupService.addMeetup(req.validatedData as CreateMeetup, id);
+            return res.status(201).json(meetup);
         } catch (err) {
             next(err);
         }
     }
 
-    async updateMeetup(req, res, next) {
+    async updateMeetup(req: AuthenticatedRequest, res: Response, next: NextFunction): Promise<Response>   {
         try {
-            const { role, accessingUserId, userId } = req.validatedData;
+            const { role, accessingUserId, userId } = req.user;
             if (role === UserRole.USER && accessingUserId !== userId) {
                 throw ApiError.Forbidden();
             }
-            const updatedMeetup = await meetupService.updateMeetup(req.validatedData);
-            res.status(200).json(updatedMeetup);
+            const updatedMeetup = await meetupService.updateMeetup(req.validatedData as UpdateMeetup);
+            return res.status(200).json(updatedMeetup);
 
         } catch (err) {
             next(err);
         }
     }
-    async deleteMeetup(req, res, next) {
+
+    async deleteMeetup(req: AuthenticatedRequest, res: Response, next: NextFunction): Promise<Response>   {
         try {
             const { role, id } = req.user;
             if (role === UserRole.USER) {
                 throw ApiError.Forbidden();
             }
             const deletedMeetup = await meetupService.deleteMeetup(req.body.id, id);
-            res.status(204).json(deletedMeetup);
+            return res.status(204).json(deletedMeetup);
         } catch (err) {
             next(err);
         }
