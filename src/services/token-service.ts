7df2aa412c15749e33ac31b7849d025642d6fb76
Varabyova Maryia <varabyovamaryia@Varabyovas-MacBook-Air.local>
2023-04-15 11:38:12 +0300

    feat: add hooks "afterFind" and "afterCreate" to Token model

--- src/services/token-service.ts
+++ src/services/token-service.ts
@@ -1,49 +1,68 @@
-import jwt from 'jsonwebtoken';
+import jwt, {JwtPayload} from 'jsonwebtoken';
+import {TokenPayload} from "../types/TokenPayload";
+import {TokenPair} from "../types/TokenPair";
+
 const { Token } = require('../models/token-model');
 
 class TokenService {
-    generateToken(payload) {
-        const accessToken = jwt.sign(payload, process.env.JWT_ACCCESS_SECRET, { expiresIn: '30m' });
-        const refreshToken = jwt.sign(payload, process.env.JWT_REFRESH_SECRET, { expiresIn: '30d' });
-        return {
-            accessToken,
-            refreshToken
+    generateToken(payload: TokenPayload): TokenPair {
+        try {
+            const accessToken = jwt.sign(payload, process.env.JWT_ACCCESS_SECRET, { expiresIn: '30m' });
+            const refreshToken = jwt.sign(payload, process.env.JWT_REFRESH_SECRET, { expiresIn: '30d' });
+            return {
+                accessToken,
+                refreshToken
+            }
+        } catch (err) {
+            throw err;
         }
     }
 
-    validateAccessToken(token) {
+    validateAccessToken(token: string): TokenPayload | null {
         try {
-            const userData = jwt.verify(token, process.env.JWT_ACCCESS_SECRET);
-            return userData;
+            const userData = jwt.verify(token, process.env.JWT_ACCCESS_SECRET) as JwtPayload;
+            const { id, email, role } = userData;
+            return { id, email, role };
         } catch (e) {
             return null;
         }
     }
 
-    validateRefreshToken(token: string) {
+    validateRefreshToken(token: string): TokenPayload | null {
         try {
-            const userData = jwt.verify(token, process.env.JWT_REFRESH_SECRET);
-            return userData;
+            const userData = jwt.verify(token, process.env.JWT_REFRESH_SECRET) as JwtPayload;
+            const { id, email, role } = userData;
+            return { id, email, role };
         } catch (e) {
             return null;
         }
     }
 
     async saveToken(userId, refreshToken) {
-        const tokenData = await Token.findOne({where: {userId}});
-        // для обновления refresh_token в бд
-        if (tokenData) {
-            tokenData.refresh_token = refreshToken;
-            return tokenData.save();
+        try {
+            const tokenData = await Token.findOne({where: {userId}});
+            // для обновления refresh_token в бд
+            if (tokenData) {
+                tokenData.refresh_token = refreshToken;
+                return tokenData.save();
+            }
+            // пользователя, который логиниться впервые
+            const token = await Token.create({ userId, refresh_token: refreshToken });
+            return token;
+        } catch (err) {
+            throw err;
         }
-        // пользователя, который логиниться впервые
-        const token = await Token.create({ userId, refresh_token: refreshToken });
-        return token;
     }
 
     async findToken(refreshToken: string) {
-        const tokenData = await Token.findOne({where : {refresh_token: refreshToken}});
-        return tokenData;
+        try {
+            const tokenData = await Token.findOne(
+                {where : {refresh_token: refreshToken}}
+            );
+            return tokenData;
+        } catch (err) {
+            throw err;
+        }
     }
 }
 
