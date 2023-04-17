3aa89f000d169d95562aa6137d595c111d06eb6d
Varabyova Maryia <varabyovamaryia@Varabyovas-MacBook-Air.local>
2023-04-17 20:59:00 +0300

    refactor: update imported entities in MeetupAfterHook file

--- src/types/MeetupAfterCreateHook.ts
+++ src/types/MeetupAfterCreateHook.ts
@@ -1,5 +1,6 @@
-import {MeetUpAttributes, MeetUpInstance} from "./MeetUpAttributes";
+import {MeetUpAttributes} from "./MeetUpAttributes";
 import {CreateOptions} from "sequelize";
+import {MeetUpInstance} from "./MeetUpInstance";
 
 export interface MeetupAfterCreateHook {
     (instance: MeetUpInstance, options: CreateOptions<MeetUpAttributes>): Promise<void> | void;
