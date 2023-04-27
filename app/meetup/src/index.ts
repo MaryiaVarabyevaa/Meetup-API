88d20cd5e1a06982617eb2330410325245beb46a
Varabyova Maryia <varabyovamaryia@Varabyovas-MacBook-Air.local>
2023-04-27 11:51:54 +0300

    init: add basic settings to meetup service of Meetup microtasts API

---
+++ app/meetup/src/index.ts
@@ -0,0 +1,12 @@
+import express from 'express';
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Hello, world!');
+});
+
+const port = process.env.PORT || 4000;
+app.listen(port, () => {
+    console.log(`Server listening on port ${port}`);
+});
