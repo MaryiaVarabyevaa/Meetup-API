a84fe7bb4edb9870c700ca566525736e86e35a5d
Varabyova Maryia <varabyovamaryia@Varabyovas-MacBook-Air.local>
2023-04-27 11:47:37 +0300

    refactor: update service's settings

--- app/auth/src/index.ts
+++ app/auth/src/index.ts
@@ -1,11 +1,12 @@
 import express from 'express';
-const app = express()
-const port = 3000
+
+const app = express();
 
 app.get('/', (req, res) => {
-    res.send('Hello World!')
-})
+    res.send('Hello, world!');
+});
 
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
-})
+    console.log(`Server listening on port ${port}`);
+});
