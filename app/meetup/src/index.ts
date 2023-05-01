6bb355f62fa8f2ff72cbaa42e3a0229b38af6b52
Varabyova Maryia <varabyovamaryia@Varabyovas-MacBook-Air.local>
2023-05-01 22:08:38 +0300

    feat: add error middleware to meetup service

--- app/meetup/src/index.ts
+++ app/meetup/src/index.ts
@@ -3,7 +3,7 @@ import express from 'express';
 import sequelize from './db/db';
 import Meetup from './db/models/meetup-model';
 import router from "./routes";
-// import { connectConsumer, disconnectConsumer } from "./kafka/kafka";
+import errorMiddleware from "./middlewares/error-middleware";
 
 dotenv.config();
 
@@ -13,19 +13,12 @@ const app = express();
 
 app.use(express.json());
 app.use('/api', router);
-
-// async function gracefulShutdown() {
-//   console.log('Shutting down');
-//   await disconnectConsumer();
-//   process.exit(0);
-// }
+app.use(errorMiddleware);
 
 const start = async () => {
   try {
     await sequelize.authenticate();
     await sequelize.sync({ alter: true });
-
-    // await connectConsumer();
 
     app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
   } catch (e) {
---
+++ app/meetup/src/middlewares/error-middleware.ts
@@ -0,0 +1,10 @@
+import { Request, Response, NextFunction } from 'express';
+import ApiError from '../exceptions/api-error';
+import { ErrorMessages } from "../constants/errorMessages";
+
+export default (err: Error, req: Request, res: Response, next: NextFunction) => {
+  if (err instanceof ApiError) {
+    return res.status(err.status).json({message: err.message})
+  }
+
+  return res.status(500).json({message: ErrorMessages.UNEXPECTED_ERROR})
