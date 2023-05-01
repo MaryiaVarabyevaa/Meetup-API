1ef65d9fad6333ae82c8c8b00dbb06e40bb284fd
Varabyova Maryia <varabyovamaryia@Varabyovas-MacBook-Air.local>
2023-05-01 22:34:48 +0300

    feat: add token service and token model to auth microservice

---
+++ app/auth/src/db/models/token-model.ts
@@ -0,0 +1,9 @@
+import { DataTypes } from "sequelize";
+import sequelize from "../db";
+
+export const Token = sequelize.define('token', {
+  id: {type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
+  refresh_token: {type: DataTypes.STRING, allowNull: false},
+});
+
+export default Token;
---
+++ app/auth/src/services/token-service.ts
@@ -0,0 +1,43 @@
+import jwt, {JwtPayload} from 'jsonwebtoken';
+import Token from "../models/token-model";
+
+class TokenService {
+  generateToken(payload) {
+    const accessToken = jwt.sign(payload, process.env.JWT_ACCCESS_SECRET || '', { expiresIn: '30m' });
+    const refreshToken = jwt.sign(payload, process.env.JWT_REFRESH_SECRET || '', { expiresIn: '30d' });
+    return {
+      accessToken,
+      refreshToken
+    }
+  }
+
+  validateAccessToken(token: string) {
+    const userData = jwt.verify(token, process.env.JWT_ACCCESS_SECRET as string) as JwtPayload;
+    const { id, email, role } = userData;
+    return { id, email, role };
+  }
+
+  validateRefreshToken(token: string) {
+    const userData = jwt.verify(token, process.env.JWT_REFRESH_SECRET as string) as JwtPayload;
+    const { id, email, role } = userData;
+    return { id, email, role };
+  }
+
+  async saveToken(userId: number, refreshToken: string) {
+    const tokenData = await Token.findOne({where: {userId}});
+    if (tokenData) {
+      tokenData.refresh_token = refreshToken;
+      await tokenData.save();
+      return;
+    }
+    const token = await Token.create({ userId, refresh_token: refreshToken });
+    return token;
+  }
+
+  async findToken(refreshToken: string) {
+    const tokenData = await Token.findOne({where : {refresh_token: refreshToken}});
+    return tokenData;
+  }
+}
+
+export default new TokenService();
