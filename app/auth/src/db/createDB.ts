bcd4402e34b7f368cc54816770db2a399a3016a5
Varabyova Maryia <varabyovamaryia@Varabyovas-MacBook-Air.local>
2023-05-02 21:12:57 +0300

    fix: update enum of user roles

--- app/auth/src/db/createDB.ts
+++ app/auth/src/db/createDB.ts
@@ -1,10 +1,9 @@
-import { Prisma } from "@prisma/client";
-import prisma from "./index";
-
+import { Prisma } from '@prisma/client';
+import { prisma } from './index';
 
 const createDB = async () => {
   try {
-    const DB_NAME = process.env.POSTGRES_DB || "AuthDB";
+    const DB_NAME = process.env.POSTGRES_DB || 'AuthDB';
     const sql = Prisma.sql`CREATE DATABASE ${Prisma.raw(DB_NAME)}`;
     await prisma.$executeRaw(sql);
     console.log(`Database ${DB_NAME} created successfully`);
@@ -13,6 +12,6 @@ const createDB = async () => {
   } finally {
     await prisma.$disconnect();
   }
-}
+};
 
 createDB();
--- app/auth/src/db/index.ts
+++ app/auth/src/db/index.ts
@@ -1,5 +1,5 @@
-import { PrismaClient } from '@prisma/client';
+import { PrismaClient, UserRole } from '@prisma/client';
 
 const prisma = new PrismaClient();
 
-export default prisma;
+export { prisma, UserRole };
