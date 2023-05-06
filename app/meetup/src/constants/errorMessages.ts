d6a5747c4b747e7d1c9e9bf0b5cff6416ffcbb9a
Varabyova Maryia <varabyovamaryia@Varabyovas-MacBook-Air.local>
2023-05-07 00:49:28 +0300

    feat: add .env files for development and production

--- app/meetup/src/constants/errorMessages.ts
+++
@@ -1,5 +0,0 @@
-export enum ErrorMessages {
-  MEETUP_NOT_FOUNT = 'The meetup with the ID you provided does not exist in the database',
-  MEETUP_CONFLICT = 'The record of meetup you are trying to create already exists in the database.',
-  UNEXPECTED_ERROR = 'An unexpected error occurred while processing your request. Please try again later.'
-}
--- app/meetup/src/routes/index.ts
+++
@@ -1,8 +0,0 @@
-import {Router} from 'express';
-import meetup from "./meetup";
-
-const router = Router();
-
-router.use('/meetup', meetup);
-
-export default router;
