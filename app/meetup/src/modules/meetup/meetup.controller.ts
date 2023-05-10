3b40deab1ae8e633ac717ca741bfc3e7b9a2e2e6
Varabyova Maryia <varabyovamaryia@Varabyovas-MacBook-Air.local>
2023-05-10 15:09:44 +0300

    refactor: delete comments in meetup.controller.ts file

--- app/meetup/src/modules/meetup/meetup.controller.ts
+++ app/meetup/src/modules/meetup/meetup.controller.ts
@@ -84,56 +84,6 @@ class MeetupController {
       return;
     }
   }
-
-  // async generateReport(req: Request, res: Response, next: NextFunction) {
-  //   try {
-  //     const { type } = req.params;
-  //     let doc: string;
-  //     if (type === "pdf") {
-  //       doc = await meetupService.generateReportPDF();
-  //     } else {
-  //       doc = await meetupService.generateReportCVS();
-  //     }
-  //     const PATH = path.join(__dirname, "../../reports", doc);
-  //
-  //     if (fs.existsSync(PATH)) {
-  //       const readStream = fs.createReadStream(PATH);
-  //       const writeStream = fs.createWriteStream(`temp_${doc}`);
-  //       const contentType = type === "pdf"? "application/pdf" : "text/csv";
-  //
-  //       readStream.pipe(writeStream);
-  //       readStream.on("error", (err) => {
-  //         console.error(err);
-  //         return res.status(500).send("Server error");
-  //       });
-  //
-  //       writeStream.on("finish", () => {
-  //         const info = fs.statSync(`temp_${doc}`);
-  //         res.setHeader("Content-Length", info.size);
-  //         res.setHeader("Content-Type", contentType);
-  //         res.setHeader("Content-Disposition", `attachment; filename=${doc}`);
-  //         const file = fs.createReadStream(`temp_${doc}`);
-  //         file.pipe(res);
-  //         file.on("error", (err) => {
-  //           console.error(err);
-  //           return res.status(500).send("Server error");
-  //         });
-  //       });
-  //
-  //       req.setTimeout(120000, () => {
-  //         console.log("Request timed out.");
-  //         readStream.destroy();
-  //         writeStream.destroy();
-  //         return res.status(408).send("Request timed out.");
-  //       });
-  //     } else {
-  //       return res.status(404).send("File not found");
-  //     }
-  //   } catch (err) {
-  //     next(err);
-  //     return;
-  //   }
-  // }
 }
 
 export default new MeetupController();
